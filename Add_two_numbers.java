724623cf068cea5a4c1955c39a370e1b61422d2d
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-08-22 21:33:44 +0530

    Add files via upload

---
+++ Add_two_numbers.java
@@ -0,0 +1,9 @@
+public class Add_two_numbers {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("Sum of two given numbers is: "+ sum);
+
+    }
+}
