cd235e4cfa8d30daf66166849a5a47b7a1f2bde7
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-08-28 15:12:36 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
+class reverse{
+    void reversearray(char[] arr){
+        int n = arr.length;
+        char[] arr1=new char[];
+        for (int i =0; i<n;i++){
+            for(int j=n;j<0;j--) {
+                arr1[i]=arr[j];
+            }
+        }
+    }
 
+}
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if (n>0){
-            System.out.println(n + " is a positive number ");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array : ");
+        int n =sc.nextInt();
+        System.out.println("Enter array : ");
+        char[] arr=new char[n];
+        for (int i=0;i<n;i++){
+            arr[i]=sc.next().charAt(0);
         }
-        else if (n<0) {
-            System.out.println(n + " is a negative number ");
+        reverse o=new reverse();
+        o.reversearray(arr);
+        for(int i = 0; i<n; i++){
+
         }
-        else {
-            System.out.println(n + " is a zero ");
-        }
+
+
+
     }
 }
