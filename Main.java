93e07fe941346ed208c7a8dee69c4ff1f15ae062
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-09-04 22:24:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+//Printing the bank balance using class object method
+
+import java.util.Scanner;
+class Bank_acc{
+    long account_number;
+    String account_name;
+    double account_balance;
+    void printBalance(){
+        System.out.println("Balance is : " + account_balance);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       Bank_acc o=new Bank_acc();
+       o.account_number=23456;
+       o.account_name="prajna";
+       o.account_balance=2345.55;
+       o.printBalance();
+
+    }
+}
