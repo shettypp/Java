a1af1c314536a8df142ff3bfe718bf33b259abd0
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-09-30 07:17:57 +0530

    Update and rename Main.java to UpperCase_lowerCase.java

--- UpperCase_lowerCase.java
+++ UpperCase_lowerCase.java
@@ -30,7 +30,7 @@ public class Main {
 
 import java.util.Scanner;
 
-public class Main {
+public class UpperCase_lowerCase {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter string : ");
