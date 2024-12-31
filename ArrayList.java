1a7ac9a2dcfe04464a2454d3f2c0670ed6870c6d
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-12-31 10:56:37 +0530

    Update and rename Main.java to ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class ArrayList {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         ArrayList<Integer> arraylist =new ArrayList<>();
