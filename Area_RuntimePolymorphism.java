286078decf259efe97699261c597a08405ef2c9d
Prajna shetty <153827006+shettypp@users.noreply.github.com>
2024-12-31 10:59:07 +0530

    Update and rename Main.java to Area_RuntimePolymorphism.java

--- Area_RuntimePolymorphism.java
+++ Area_RuntimePolymorphism.java
@@ -35,7 +35,7 @@ class Circle_area extends Area{
         System.out.println("Area of circle is : "+ area);
     }
 }
-public class Main {
+public class Area_RuntimePolymorphism {
     public static void main(String[] args) {
         Area r=new Rectangle_area();
         r.display(); // overriding
